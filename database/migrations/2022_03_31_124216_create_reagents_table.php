ace0b45a6461810e991daa524d6b8de8ad8b3b5d
MinemuraKazumi <mine.mine.3.7.11@gmail.com>
2022-04-14 20:52:51 +0900

    “laravel-yakuhin-kanri”

---
+++ database/migrations/2022_03_31_124216_create_reagents_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('reagents', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('reagent_name');//文字：薬品名
+            $table->integer('reagent_number');//数字：薬品本数
+            $table->integer('reagent_capcity');//数字：薬品容量
+            $table->integer('reagent_barcode');//数字：薬品のバーコード
+            $table->string('reagent_epuipment');//文字：保護具
+            $table->string('reagent_correspondence');//文字：緊急対応
+            $table->string('reagent_storage');//文字：保護具
+            $table->string('reagent_document');//文字：SDS
+            $table->datetime('published');//
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('reagents');
+    }
+};
