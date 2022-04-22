40932b5da9f6bef0cd8dc42e085cbb610258549a
EC2 Default User <ec2-user@ip-172-31-74-212.ec2.internal>
2022-04-22 15:11:10 +0000

    placeholder

--- resources/views/reagents.blade.php
+++ resources/views/reagents.blade.php
@@ -20,7 +20,7 @@
              </div>
              <div class="form-group">
                  <div class="col-sm-6">
-                     <input type="text" name="reagent_name" class="form-control" placeholder="薬品名を入力してください">
+                     <input type="text" name="reagent_name" class="form-control" placeholder="薬品名 / 未開封容量を入力してください">
                  </div>
              </div>
 
@@ -37,7 +37,7 @@
               </div>
              <div class="form-group">
                  <div class="col-sm-6">
-                     <input type="text" name="reagent_capcity" class="form-control" placeholder="容量を入力してください">
+                     <input type="text" name="reagent_capcity" class="form-control" placeholder="重量を整数で入力してください">
                  </div>
              </div>
              <div class="card-title" style="padding-top:10px;">
